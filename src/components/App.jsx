5373f4751488ee435e52fc3697bad2e5411ef7fd
Zhuk Roman <95108009+Roman90000@users.noreply.github.com>
2024-01-10 22:59:51 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,7 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#b72323',
       }}
     >
       React homework template
